d945f5913171db260fce90e82b57ca548b0aa4eb
Ruchir Dakhale <142479189+ruchir-2006@users.noreply.github.com>
2024-09-25 22:36:10 +0530

    Add files via upload

---
+++ Check_if_String_is_a_rotation_ofString.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n =s.size();
+        int m = goal.size();
+        if(n!=m)
+        return false;
+
+       string temp = s + s;
+       if(temp.find(goal)!= string :: npos)
+       {
+       return true; 
+       }
+       return false;
+        
+    }
+};
